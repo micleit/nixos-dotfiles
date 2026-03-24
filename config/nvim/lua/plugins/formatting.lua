2a016d5d87e2b56e27d258b911b63c555ddb6e00
micah leiterman <micah.leiterman@gmail.com>
2026-03-24 13:31:16 -0400

    completions nvim

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -1,6 +1,8 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
 		conform.setup({
 			formatters_by_ft = {
@@ -10,24 +12,26 @@ return {
 				typescript = { "prettier" },
 				html = { "prettier" },
 				css = { "prettier" },
-				-- Java usually uses the LSP (jdtls) for formatting
 				java = { "google-java-format" },
 			},
-			-- Optional: Format on save
-			-- format_on_save = {
-			--   lsp_fallback = true,
-			--   async = false,
-			--   timeout_ms = 500,
-			-- },
+			-- Set global formatting options
+			default_format_opts = {
+				lsp_fallback = true,
+			},
 		})
 
-		-- THE KEYBIND: <leader>f to format
+		-- THE KEYBIND: <leader>fd to format with shiftwidth=2
 		vim.keymap.set({ "n", "v" }, "<leader>fd", function()
+			-- Temporarily set buffer options for the format call
+			vim.opt_local.shiftwidth = 4
+			vim.opt_local.tabstop = 4
+			vim.opt_local.softtabstop = 4
+
 			conform.format({
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 1000,
 			})
-		end, { desc = "Format file or range (in visual mode)" })
+		end, { desc = "Format file or range" })
 	end,
 }
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,39 +1,60 @@
 return {
-  -- The Main Completion Plugin
-  {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is very old
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp") -- This is what was failing to load
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          -- ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		-- 1. DEFINE YOUR SERVERS (Modern 0.11+ Style)
+		-- We define the configuration for each server using vim.lsp.config
+		local servers = {
+			basedpyright = {},
+			texlab = {},
+			html = {},
+			ts_ls = {},
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+					},
+				},
+			},
+		}
+
+		-- 2. ENABLE SERVERS
+		-- This replaces the old lspconfig[name].setup() framework
+		for name, config in pairs(servers) do
+			config.capabilities = vim.tbl_deep_extend("force", capabilities, config.capabilities or {})
+			vim.lsp.enable(name, config)
+		end
+
+		-- 3. DIAGNOSTIC CONFIG (Same as before)
+		vim.diagnostic.config({
+			virtual_text = false, -- Start with it OFF
+			signs = true,
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+		})
+
+		-- 4. THE TOGGLE: <leader>vn
+		vim.keymap.set("n", "<leader>vn", function()
+			local config = vim.diagnostic.config().virtual_text
+			if config then
+				vim.diagnostic.config({ virtual_text = false })
+				print("LSP Virtual Text: OFF")
+			else
+				vim.diagnostic.config({
+					virtual_text = { prefix = "●", spacing = 4 },
+				})
+				print("LSP Virtual Text: ON")
+			end
+		end, { desc = "Toggle LSP Virtual Text" })
+
+		-- 5. GLOBAL LSP KEYMAPS
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+	end,
 }
