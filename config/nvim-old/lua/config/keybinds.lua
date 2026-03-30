8b1b83d017186462c71a4efdb7e60ea5c494d674
micah leiterman <micah.leiterman@gmail.com>
2026-03-30 07:09:26 -0400

    cleaning up

--- config/nvim-old/lua/config/keybinds.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
