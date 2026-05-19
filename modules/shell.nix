a04e308182828e02ba9a8fd79dfd88712418b3d0
micah leiterman <micah.leiterman@gmail.com>
2026-05-19 11:27:14 -0400

    wallpaper + noctalia update

--- modules/shell.nix
+++ modules/shell.nix
@@ -12,6 +12,30 @@
 
       set -gx fish_greeting ""
 
+      function bind_bang
+          switch (commandline -t)[-1]
+              case "!"
+                  commandline -t -- $history[1]
+                  commandline -f repaint
+              case "*"
+                  commandline -i !
+          end
+      end
+
+      function bind_dollar
+          switch (commandline -t)[-1]
+              case "!"
+                  commandline -f backward-delete-char history-token-search-backward
+              case "*"
+                  commandline -i '$'
+          end
+      end
+
+      function fish_user_key_bindings
+          bind ! bind_bang
+          bind '$' bind_dollar
+      end
+
 
       if status is-interactive
           if type -q nerdfetch
